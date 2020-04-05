95e394a16fc56ecfc1c6644920d430665247a4eb
Leila Costa Ferreira <leila.costa.ferreira@avanade.com>
2020-04-05 18:12:17 -0300

    commit inicial

---
+++ README.md
@@ -0,0 +1,28 @@
+#### **Desafio Codenation: Criptografia de Júlio César**
+Segundo o Wikipedia, criptografia ou criptologia (em grego: kryptós, “escondido”, e gráphein, “escrita”) é o estudo e prática de princípios e técnicas para comunicação segura na presença de terceiros, chamados “adversários”. Mas geralmente, a criptografia refere-se à construção e análise de protocolos que impedem terceiros, ou o público, de lerem mensagens privadas. Muitos aspectos em segurança da informação, como confidencialidade, integridade de dados, autenticação e não-repúdio são centrais à criptografia moderna. Aplicações de criptografia incluem comércio eletrônico, cartões de pagamento baseados em chip, moedas digitais, senhas de computadores e comunicações militares. Das Criptografias mais curiosas na história da humanidade podemos citar a criptografia utilizada pelo grande líder militar romano Júlio César para comunicar com os seus generais. Essa criptografia se baseia na substituição da letra do alfabeto avançado um determinado número de casas. Por exemplo, considerando o número de casas = 3:
+
+**Normal: a ligeira raposa marrom saltou sobre o cachorro cansado**
+
+**Cifrado: d oljhlud udsrvd pduurp vdowrx vreuh r fdfkruur fdqvdgr**
+
+#### Regras
+As mensagens serão convertidas para minúsculas tanto para a criptografia quanto para descriptografia.
+No nosso caso os números e pontos serão mantidos, ou seja:
+- Normal: 1a.a
+
+- Cifrado: 1d.d
+
+Escrever programa, em qualquer linguagem de programação, que faça uma requisição HTTP para a url abaixo:
+
+https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=7a3969cbcc527c4d442d86d63af3d60352fd6868
+🔧 Tecnologias
+- PHP
+- json
+
+ℹ️ Como usar
+Antes de iniciar :
+- xammp ou PHP e TOMCAT
+
+
+Autor
+Leila Ferreira - LeilaFerreira
---
+++ Teste.php
@@ -0,0 +1,61 @@
+<html>
+<head></head>
+<title></title>
+
+<?php
+
+    // $value = 110; 
+    // $index = ($value - 97 - 1) % 26;
+    // if($index < 0) $deciphered += g.fromCharCode(123 + index);
+    // echo $index ;
+    $token_id = '7a3969cbcc527c4d442d86d63af3d60352fd6868';
+    $url = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=".$token_id;
+    
+    $client = curl_init($url);
+   
+    curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+    $response = curl_exec($client);
+    $arquivo = fopen("answer.json", "x+" );
+    fwrite($arquivo, $response);
+    fclose($arquivo);
+    $conteudo = file_get_contents("answer.json");
+    $json = json_decode($conteudo);
+    $texto = strtolower($json->cifrado);
+    $array = str_split($texto);
+    $decifrado = '';
+    foreach ($array as $key => $value) {
+      $valor =  ord($value);
+      if($valor >= 97 && $valor <= 122){
+        $decifrado .= chr($valor - $json->numero_casas);
+      }else{
+        $decifrado .= chr($valor);
+      }
+      
+    }
+   
+    echo   $decifrado;
+     $json->decifrado = $decifrado;
+     $json->resumo_criptografico = sha1($decifrado); 
+     $novoArquivo = fopen("answer.json", "r+" );
+     fwrite($novoArquivo, json_encode($json));
+     fclose($novoArquivo);
+
+
+     $ch = curl_init('https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token='.$token_id); 
+     $headers = array("Content-Type:multipart/form-data"); 
+     curl_setopt($ch, CURLOPT_POST,1);
+     curl_setopt_array($ch, 
+     [ 
+     CURLOPT_RETURNTRANSFER => true, 
+     CURLOPT_POST => true, 
+     CURLOPT_HTTPHEADER => $headers,
+     CURLOPT_POSTFIELDS => [ 'answer' => curl_file_create('answer.json') ] ]); 
+    
+     $resposta = curl_exec($ch); 
+     $err = curl_error($ch);
+     echo $err;
+     echo $resposta;
+     curl_close($ch);
+    
+?>
+</html>
